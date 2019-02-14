8bfa6c4fc5cba9aa569b471762a62394629bff74
aurokp2 <aurokp2@gmail.com>
2019-02-13 22:25:14 -0800

    Added option to Utility.R

--- R/Utility.R
+++ R/Utility.R
@@ -1,8 +1,24 @@
 #**************************************************************************
 #
 #
+#TFDEA_V2 Package
+#
+# Author: Aurobindh Kalathil Puthanpura
+# Revision: 1
+# Date: 2019-13-09
 #
 #
-#
+# U t i l i t y f i l e
+# internal use only
 #
 #**************************************************************************
+#
+# Package Global variables
+#
+
+#
+# Options Strings
+#
+
+options.orientation.l <- c ("input", "output")
+options.rts.l <- c ("vrs" , "crs")
